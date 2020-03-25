1abec2780ee6a148df65cea6c903707dfbfd4607
ColorlessSmell <62539812+ColorlessSmell@users.noreply.github.com>
2020-03-25 06:33:36 +0400

    comparator changes

--- Assignment8/src/ClubNameComparator.java
+++ Assignment8/src/ClubNameComparator.java
@@ -2,15 +2,23 @@ import java.util.Comparator;
 
 public class ClubNameComparator implements Comparator<Club>{
 
+    private String c1Name;
+    private String c2Name;
+
     //this is the abstract method inside the Comparator interface
     public int compare(Club c1, Club c2)
     {
+        int result = 0;
         //compare their last names first, if last name same, then
         //compare their first names
-        String c1Name = c1.getClubName();
-        String c2Name = c2.getClubName();
+        if (c1 != null && c2 != null) {
+            c1Name = c1.getClubName();
+            c2Name = c2.getClubName();
+            result = c1Name.compareTo(c2Name);
+        }
 
-        return c1Name.compareTo(c2Name);
+        return result;
+
 
 
     }
--- Assignment8/src/MemberNumberComparator.java
+++ Assignment8/src/MemberNumberComparator.java
@@ -2,15 +2,18 @@ import java.util.Comparator;
 
 public class MemberNumberComparator implements Comparator<Club>{
 
+    private int result = 0;
     //this is the abstract method inside the Comparator interface
     public int compare(Club c1, Club c2)
     {
         //compare their last names first, if last name same, then
         //compare their first names
-        int club1 = c1.getNumberOfMembers();
-        int club2 = c2.getNumberOfMembers();
+        if (c1 != null && c2 != null) {
+            int club1 = c1.getNumberOfMembers();
+            int club2 = c2.getNumberOfMembers();
 
-        int result = club1 - club2;
+            result = club1 - club2;
+        }
 
         return result;
 
