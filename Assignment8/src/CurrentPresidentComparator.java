6fda3ae04cbb76e31490ecffeda15ed121c97741
ColorlessSmell <62539812+ColorlessSmell@users.noreply.github.com>
2020-03-25 08:52:01 +0400

    NullPointer fixes

--- Assignment8/src/CurrentPresidentComparator.java
+++ Assignment8/src/CurrentPresidentComparator.java
@@ -2,21 +2,40 @@ import java.util.Comparator;
 
 public class CurrentPresidentComparator implements Comparator<President>{
 
+    private String lNamep1;
+    private String lNamep2;
+    private int result;
+
     public int compare(President p1, President p2){
 
-        String lNamep1 = p1.getLastName();
-        String lNamep2 = p2.getLastName();
 
-        if (lNamep1.equals(lNamep2)) {
+        if (p1 != null && p2 != null) {
+            lNamep1 = p1.getLastName();
+            lNamep2 = p2.getLastName();
+        } else if (p1 == null && p2 != null){
+
+            result = -1;
+
+        } else if (p1 != null && p2 == null){
+
+            result = 1;
+
+        } else if(p1 == null && p2 == null){
+
+            result = 0;
+
+        } else if (lNamep1.equals(lNamep2)) {
 
             String fNamep1 = p1.getFirstName();
             String fNamep2 = p2.getFirstName();
 
-            return fNamep1.compareTo(fNamep2);
+            result = fNamep1.compareTo(fNamep2);
 
-        } else {
-            return lNamep1.compareTo(lNamep2);
+        } else if (!lNamep1.equals(lNamep2)){
+            result = lNamep1.compareTo(lNamep2);
         }
+
+        return result;
     }
 
 }
